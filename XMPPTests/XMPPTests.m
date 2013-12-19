16df8c5b130be663cec9ccf6c34af1e8312d3bd1
jonathan <jonathan@jonathan.local>
2013-12-19 13:16:52 +0800

    project

---
+++ XMPPTests/XMPPTests.m
@@ -0,0 +1,34 @@
+//
+//  XMPPTests.m
+//  XMPPTests
+//
+//  Created by jonathan on 13-12-19.
+//  Copyright (c) 2013年 jonathan. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface XMPPTests : XCTestCase
+
+@end
+
+@implementation XMPPTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
